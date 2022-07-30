53a9586618fc2e32756c9d2a48b4a4674f54daac
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2022-07-30 12:41:44 +0200

    :recycle: use ok() for type conversions

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,15 +1,11 @@
 use std::ops::Range;
 
 pub fn bytes_to_string(bytes: &[u8], range: Range<usize>) -> Option<String> {
-    match String::from_utf8(bytes[range].to_vec()) {
-        Ok(val) => Some(val),
-        _ => None,
-    }
+    String::from_utf8(bytes[range].to_vec()).ok()
 }
 
 pub fn bytes_to_usize_be(bytes: &[u8], range: Range<usize>) -> Option<usize> {
-    match bytes[range].try_into() {
-        Ok(result) => Some(usize::from_be_bytes(result)),
-        Err(_) => None,
-    }
+    let u_bytes = bytes[range].try_into().ok()?;
+
+    Some(usize::from_be_bytes(u_bytes))
 }
