ad2c1ae9f211b9efdaedffcce944b02f2c7446b0
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2022-08-14 21:35:49 +0200

    :fire: cleanup raw parsing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use rawlib::exif;
-use rawlib::fuji;
 use std::env;
 use std::fs;
 
@@ -7,27 +6,6 @@ fn parse_path(args: Vec<String>) -> String {
     let resolved = args.get(1);
 
     resolved.expect("path was not provided").to_string()
-}
-
-fn jpeg_only(args: Vec<String>) -> bool {
-    let resolved = args.get(2);
-
-    matches!(resolved, Some(_))
-}
-
-fn process_raw(file: Vec<u8>, resolved: String) {
-    let result = crate::fuji::parse(&file).expect("result does not exist");
-
-    println!(
-        "{} {} {} {}",
-        result.format, result.identifier, result.model, result.version
-    );
-
-    let jpeg_path = format!("{}.jpeg", &resolved);
-
-    fs::write(&jpeg_path, &result.jpeg.bytes).expect("failed to write jpeg");
-
-    println!("jpeg written to {}", &jpeg_path);
 }
 
 fn process_jpeg(file: &[u8]) {
@@ -44,13 +22,8 @@ fn process_jpeg(file: &[u8]) {
 
 fn main() {
     let resolved = parse_path(env::args().collect());
-    let is_jpeg = jpeg_only(env::args().collect());
 
     let file = fs::read(&resolved).unwrap();
 
-    if is_jpeg {
-        process_jpeg(&file);
-    } else {
-        process_raw(file, resolved);
-    }
+    process_jpeg(&file);
 }
